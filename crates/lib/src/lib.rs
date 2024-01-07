bb7d2e7e23a7161a761ae394d66a9f181a84a732
tqkoh <shktmygs@gmail.com>
2024-01-08 05:27:29 +0900

    update doc

--- crates/lib/src/lib.rs
+++ crates/lib/src/lib.rs
@@ -11,7 +11,7 @@
 //! Rust 競プロ用ライブラリ。あんま追加してないけど [C++ 版](https://tqk.blue/library/) もある
 //!
 //! - [repo](https://github.com/tqkoh/library-rs)
-//! - [verify log](verify-log.txt) いつか見た目整えてこのページとかに乗るようにする
+//! - [verify log](https://tqk.blue/library-rs/verify-log.txt) いつか見た目整えてこのページとかに乗るようにする
 //!
 
 pub mod math;
