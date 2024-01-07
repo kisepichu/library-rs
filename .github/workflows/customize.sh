ef8be547a9b9482885244bc0c1ba8067a8efbdbd
tqkoh <shktmygs@gmail.com>
2024-01-07 19:01:25 +0900

    Add index.md

--- .github/workflows/customize.sh
+++ .github/workflows/customize.sh
@@ -1,13 +1,1 @@
 echo $(pwd)
-
-# index.md
-#  ...
-#  "  verificationCategories:"
-# +"    - name: 'Verify Log'"
-# +"      pages:"
-# +"        - icon: ':heavy_check_mark:'"
-# +"          path: 'verify/log'"
-# +"          title: 'verify/log'"
-# ...
-
-sed -i -e '/verificationCategories:/a\    - name: "Verify Log"\n      pages:\n        - icon: ":heavy_check_mark:"\n          path: "verify/log"\n          title: "verify/log"' index.md
---
+++ .verify-helper/docs/index.md
@@ -0,0 +1,6 @@
+競技プログラミング用ライブラリ  
+
+全然追加してないけど C++ 版もある: [library](https://tqk.blue/library/)
+
+[Verify log](https://tqk.blue/library-rs/verify/log.txt)
+[rustdoc](https://tqk.blue/library-rs/rustdoc/)
